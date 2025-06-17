b2d20e28b4e479ec1da719516591a52869e639d3
Trung Le <126436376+trungleviet-agilityio@users.noreply.github.com>
2025-06-17 18:36:33 +0700

    Feature/test deployment (#23)
    * test: add deployment test comment
    
    * fix(ci): copy the dockerfile to vps
    
    * fix(ci): revert the old flow
    
    * fix(ci: correct name variable
    
    * fix(ci: correct name variable
    
    * fix: add the port for testing, need remove soon

--- docker-practice/core/settings/local.py
+++ docker-practice/core/settings/local.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-your-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "103.249.201.227"]
 
 # Database
 DATABASES = {
